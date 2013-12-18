842bf5837605dcbb3dc740370098068a3098e977
Sebastian Sdorra <s.sdorra@ostfalia.de>
2013-12-18 15:06:22 +0100

    update sample and docs

--- docs/js/docs-setup.js
+++ docs/js/docs-setup.js
@@ -8,14 +8,14 @@ NG_DOCS={
       "id": "adf.dashboard",
       "shortName": "dashboard",
       "type": "object",
-      "keywords": "adf all and api dashboard holds object structures the widgets"
+      "keywords": "adf all and api dashboard holds object self structures the widgets"
     },
     {
       "section": "api",
       "id": "adf.dashboardProvider",
       "shortName": "dashboardProvider",
       "type": "object",
-      "keywords": "$injector above adf after alias all an and angularjs any api are as associated be before but can closed controller created dashboardprovider default dependencies dependency edit factory fn for function html http if injected instantiated into invoke is it its key map method mode module modus name new newly object of one optional or org otherwise passed path promise promises properties register registered registeres rejected reload reloaded resolve resolved result return same scope self service should string structure structures successfully template templateurl that the them then these title to treated true used value values wait which widget widgets will with"
+      "keywords": "$injector above adf after alias all an and angularjs any api are as associated be before but can changes class closed column columns controller created css dashboard dashboardprovider default dependencies dependency displayed edit factory fn for function html http if injected instantiated into invoke is it its key loading loadingtemplate long map messages messagetemplate method mode module modus name new newly not object of one optional or org otherwise passed path promise promises properties register registered registeres rejected reload reloaded resolve resolved resources result return row rows same scope self service should string structure structures successfully template templateurl that the them then these title to treated true used value values wait which widget widgets will with"
     },
     {
       "section": "api",
