9554256a6fd1b85910c18b6ef5b61dbaed4c47e9
Sebastian Sdorra <s.sdorra@ostfalia.de>
2013-12-18 08:51:53 +0100

    initial import of gh-pages

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,36 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "adf.dashboard",
+      "shortName": "dashboard",
+      "type": "object",
+      "keywords": "adf all and api dashboard holds object structures the widgets"
+    },
+    {
+      "section": "api",
+      "id": "adf.dashboardProvider",
+      "shortName": "dashboardProvider",
+      "type": "object",
+      "keywords": "$injector above adf after alias all an and angularjs any api are as associated be before but can closed controller created dashboardprovider default dependencies dependency edit factory fn for function html http if injected instantiated into invoke is it its key map method mode module modus name new newly object of one optional or org otherwise passed path promise promises properties register registered registeres rejected reload reloaded resolve resolved result return same scope self service should string structure structures successfully template templateurl that the them then these title to treated true used value values wait which widget widgets will with"
+    },
+    {
+      "section": "api",
+      "id": "adf.directive:adfDashboard",
+      "shortName": "adfDashboard",
+      "type": "directive",
+      "keywords": "adf api dashboard directive div eca renders the which"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
