8927bc4629574f2ddcf0da00bd2f9b24bd058abd
Sebastian Sdorra <s.sdorra@ostfalia.de>
2014-03-25 20:36:59 +0100

    update sample to latest version

--- docs/js/docs-setup.js
+++ docs/js/docs-setup.js
@@ -15,14 +15,14 @@ NG_DOCS={
       "id": "adf.dashboardProvider",
       "shortName": "dashboardProvider",
       "type": "object",
-      "keywords": "$injector above adf after alias all an and angularjs any api are as associated be before but can changes class closed column columns controller created css dashboard dashboardprovider default dependencies dependency displayed edit factory fn for function html http if injected instantiated into invoke is it its key loading loadingtemplate long map messages messagetemplate method mode module modus name new newly not object of one optional or org otherwise passed path promise promises properties register registered registeres rejected reload reloaded resolve resolved resources result return row rows same scope self service should string structure structures successfully template templateurl that the them then these title to treated true used value values wait which widget widgets will with"
+      "keywords": "$injector above adf after alias all an and angularjs any api are as associated be before but can changes class closed column columns config configuration controller created css dashboard dashboardprovider default dependencies dependency description displayed edit factory fn for function html http if injected instantiated into invoke is it its key loading loadingtemplate long map messages messagetemplate method mode module modus name new newly not object of one optional or org otherwise passed path predefined promise promises properties register registered registeres rejected reload reloaded resolve resolved resources result return row rows same scope self service should string structure structures successfully template templateurl that the them then these title to treated true used value values wait which widget widgets will with"
     },
     {
       "section": "api",
       "id": "adf.directive:adfDashboard",
       "shortName": "adfDashboard",
       "type": "directive",
-      "keywords": "adf api dashboard directive div eca renders the which"
+      "keywords": "adf adfdashboard all api attribute be can components dashboard directive div eca is its model name of renders requires store the to used which with"
     }
   ],
   "apis": {
