684b0533008bfbf9084c711c1e26646eb16aef70
Ryan Conaway <ryan.conaway@decisionsource.com>
2015-01-09 13:52:41 -0600

    add row and column template variables to module

--- src/scripts/adf.js
+++ src/scripts/adf.js
@@ -26,4 +26,6 @@
 
 angular.module('adf', ['adf.provider', 'ui.bootstrap', 'ui.sortable'])
   .value('adfTemplatePath', '../src/templates/')
+  .value('rowTemplate', '<adf-dashboard-row row="row" ng-repeat="row in column.rows" edit-mode="{{editMode}}" sortable-options="sortableOptions"></adf-dashboard-row>')
+  .value('columnTemplate', '<adf-dashboard-column column="column" ng-repeat="column in row.columns" edit-mode="{{editMode}}" sortable-options="sortableOptions"></adf-dashboard-column>')
   .value('adfVersion', '<<adfVersion>>');
