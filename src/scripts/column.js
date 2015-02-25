f483619b6c137605477706adc21db69ae49f1c88
Sebastian Sdorra <s.sdorra@gmail.com>
2015-02-25 21:24:02 +0100

    indent

--- src/scripts/column.js
+++ src/scripts/column.js
@@ -25,39 +25,39 @@
 
 /* global angular */
 angular.module('adf')
-    .directive('adfDashboardColumn', function ($compile, adfTemplatePath, rowTemplate) {
-        'use strict';
+  .directive('adfDashboardColumn', function ($compile, adfTemplatePath, rowTemplate) {
+    'use strict';
 
-        function stringToBoolean(string){
-            switch(string !== null ? string.toLowerCase() : null){
-                case "true": case "yes": case "1": return true;
-                case "false": case "no": case "0": case null: return false;
-                default: return Boolean(string);
-            }
+    function stringToBoolean(string){
+      switch(string !== null ? string.toLowerCase() : null){
+        case "true": case "yes": case "1": return true;
+        case "false": case "no": case "0": case null: return false;
+        default: return Boolean(string);
+      }
+    }
+
+    return {
+      restrict: "E",
+      replace: true,
+      scope: {
+        column: "=",
+        editMode: "@",
+        sortableOptions: "=",
+        collapsible: "="
+      },
+      templateUrl: adfTemplatePath + "dashboard-column.html",
+      link: function ($scope, $element, $attr) {
+        // pass edit mode
+        $attr.$observe('editMode', function (value) {
+          $scope.editMode = stringToBoolean(value);
+        });
+
+        if (angular.isDefined($scope.column.rows) && angular.isArray($scope.column.rows)) {
+          // be sure to tell Angular about the injected directive and push the new row directive to the column
+          $compile(rowTemplate)($scope, function (cloned) {
+            $element.append(cloned);
+          });
         }
-
-        return {
-            restrict: "E",
-            replace: true,
-            scope: {
-                column: "=",
-                editMode: "@",
-                sortableOptions: "=",
-                collapsible: "="
-            },
-            templateUrl: adfTemplatePath + "dashboard-column.html",
-            link: function ($scope, $element, $attr) {
-                // pass edit mode
-                $attr.$observe('editMode', function (value) {
-                    $scope.editMode = stringToBoolean(value);
-                });
-
-                if (angular.isDefined($scope.column.rows) && angular.isArray($scope.column.rows)) {
-                    // be sure to tell Angular about the injected directive and push the new row directive to the column
-                    $compile(rowTemplate)($scope, function (cloned) {
-                        $element.append(cloned);
-                    });
-                }
-            }
-        };
-    });
+      }
+    };
+  });
