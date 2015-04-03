c9613212879f2e193c48b5c93e698971c34edda6
Sebastian Sdorra <s.sdorra@gmail.com>
2015-04-03 17:21:09 +0200

    change order of template attributes

--- src/scripts/adf.js
+++ src/scripts/adf.js
@@ -26,6 +26,6 @@
 
 angular.module('adf', ['adf.provider', 'ui.bootstrap'])
   .value('adfTemplatePath', '../src/templates/')
-  .value('rowTemplate', '<adf-dashboard-row row="row" adf-model="adfModel" collapsible="collapsible" ng-repeat="row in column.rows" edit-mode="editMode" />')
-  .value('columnTemplate', '<adf-dashboard-column column="column" adf-model="adfModel" collapsible="collapsible" ng-repeat="column in row.columns" edit-mode="editMode" />')
+  .value('rowTemplate', '<adf-dashboard-row row="row" adf-model="adfModel" collapsible="collapsible" edit-mode="editMode" ng-repeat="row in column.rows" />')
+  .value('columnTemplate', '<adf-dashboard-column column="column" adf-model="adfModel" collapsible="collapsible" edit-mode="editMode" ng-repeat="column in row.columns" />')
   .value('adfVersion', '<<adfVersion>>');
