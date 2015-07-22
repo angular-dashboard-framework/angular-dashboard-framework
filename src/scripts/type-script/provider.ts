4d4c181d6acd5a13859f429cf84daad512891362
scknechaev <k.nechaev@streamcrease.com>
2015-07-22 17:15:46 +0300

    Add files provider.ts, row.ts, widget-content.ts, widget.ts

---
+++ src/scripts/type-script/provider.ts
@@ -0,0 +1,230 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2015, Sebastian Sdorra
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+'use strict';
+
+/**
+ * @ngdoc object
+ * @name adf.dashboardProvider
+ * @description
+ *
+ * The dashboardProvider can be used to register structures and widgets.
+ */
+angular.module('adf.provider', [])
+  .provider('dashboard', function(){
+
+    var widgets: any = {};
+    var widgetsPath: string = '';
+    var structures: any = {};
+    var messageTemplate: string = '<div class="alert alert-danger">{}</div>';
+    var loadingTemplate: string = '\
+      <div class="progress progress-striped active">\n\
+        <div class="progress-bar" role="progressbar" style="width: 100%">\n\
+          <span class="sr-only">loading ...</span>\n\
+        </div>\n\
+      </div>';
+
+   /**
+    * @ngdoc method
+    * @name adf.dashboardProvider#widget
+    * @methodOf adf.dashboardProvider
+    * @description
+    *
+    * Registers a new widget.
+    *
+    * @param {string} name of the widget
+    * @param {object} widget to be registered.
+    *
+    *   Object properties:
+    *
+    *   - `title` - `{string=}` - The title of the widget.
+    *   - `description` - `{string=}` - Description of the widget.
+    *   - `config` - `{object}` - Predefined widget configuration.
+    *   - `controller` - `{string=|function()=}` - Controller fn that should be
+    *      associated with newly created scope of the widget or the name of a
+    *      {@link http://docs.angularjs.org/api/angular.Module#controller registered controller}
+    *      if passed as a string.
+    *   - `controllerAs` - `{string=}` - A controller alias name. If present the controller will be
+    *      published to scope under the `controllerAs` name.
+    *   - `template` - `{string=|function()=}` - html template as a string.
+    *   - `templateUrl` - `{string=}` - path to an html template.
+    *   - `reload` - `{boolean=}` - true if the widget could be reloaded. The default is false.
+    *   - `resolve` - `{Object.<string, function>=}` - An optional map of dependencies which should
+    *      be injected into the controller. If any of these dependencies are promises, the widget
+    *      will wait for them all to be resolved or one to be rejected before the controller is
+    *      instantiated.
+    *      If all the promises are resolved successfully, the values of the resolved promises are
+    *      injected.
+    *
+    *      The map object is:
+    *      - `key` – `{string}`: a name of a dependency to be injected into the controller.
+    *      - `factory` - `{string|function}`: If `string` then it is an alias for a service.
+    *        Otherwise if function, then it is {@link http://docs.angularjs.org/api/AUTO.$injector#invoke injected}
+    *        and the return value is treated as the dependency. If the result is a promise, it is
+    *        resolved before its value is injected into the controller.
+    *   - `edit` - `{object}` - Edit modus of the widget.
+    *      - `controller` - `{string=|function()=}` - Same as above, but for the edit mode of the widget.
+    *      - `template` - `{string=|function()=}` - Same as above, but for the edit mode of the widget.
+    *      - `templateUrl` - `{string=}` - Same as above, but for the edit mode of the widget.
+    *      - `resolve` - `{Object.<string, function>=}` - Same as above, but for the edit mode of the widget.
+    *      - `reload` - {boolean} - true if the widget should be reloaded, after the edit mode is closed.
+    *        Default is true.
+    *
+    * @returns {Object} self
+    */
+    this.widget = function(name: string, widget: any): any {
+      var w = angular.extend({reload: false}, widget);
+      if ( w.edit ){
+        var edit = {reload: true};
+        angular.extend(edit, w.edit);
+        w.edit = edit;
+      }
+      widgets[name] = w;
+      return this;
+    };
+
+    /**
+     * @ngdoc method
+     * @name adf.dashboardProvider#widgetsPath
+     * @methodOf adf.dashboardProvider
+     * @description
+     *
+     * Sets the path to the directory which contains the widgets. The widgets
+     * path is used for widgets with a templateUrl which contains the
+     * placeholder {widgetsPath}. The placeholder is replaced with the
+     * configured value, before the template is loaded, but the template is
+     * cached with the unmodified templateUrl (e.g.: {widgetPath}/src/widgets).
+     * The default value of widgetPaths is ''.
+     *
+     *
+     * @param {string} path to the directory which contains the widgets
+     *
+     * @returns {Object} self
+     */
+    this.widgetsPath = function(path: string): any {
+      widgetsPath = path;
+      return this;
+    };
+
+   /**
+    * @ngdoc method
+    * @name adf.dashboardProvider#structure
+    * @methodOf adf.dashboardProvider
+    * @description
+    *
+    * Registers a new structure.
+    *
+    * @param {string} name of the structure
+    * @param {object} structure to be registered.
+    *
+    *   Object properties:
+    *
+    *   - `rows` - `{Array.<Object>}` - Rows of the dashboard structure.
+    *     - `styleClass` - `{string}` - CSS Class of the row.
+    *     - `columns` - `{Array.<Object>}` - Columns of the row.
+    *       - `styleClass` - `{string}` - CSS Class of the column.
+    *
+    * @returns {Object} self
+    */
+    this.structure = function(name: string, structure: any): any {
+      structures[name] = structure;
+      return this;
+    };
+
+   /**
+    * @ngdoc method
+    * @name adf.dashboardProvider#messageTemplate
+    * @methodOf adf.dashboardProvider
+    * @description
+    *
+    * Changes the template for messages.
+    *
+    * @param {string} template for messages.
+    *
+    * @returns {Object} self
+    */
+    this.messageTemplate = function(template: string): any {
+      messageTemplate = template;
+      return this;
+    };
+
+   /**
+    * @ngdoc method
+    * @name adf.dashboardProvider#loadingTemplate
+    * @methodOf adf.dashboardProvider
+    * @description
+    *
+    * Changes the template which is displayed as
+    * long as the widget resources are not resolved.
+    *
+    * @param {string} loading template
+    *
+    * @returns {Object} self
+    */
+    this.loadingTemplate = function(template: string): any {
+      loadingTemplate = template;
+      return this;
+    };
+
+   /**
+    * @ngdoc service
+    * @name adf.dashboard
+    * @description
+    *
+    * The dashboard holds all options, structures and widgets.
+    *
+    * @property {Array.<Object>} widgets Array of registered widgets.
+    * @property {string} widgetsPath Default path for widgets.
+    * @property {Array.<Object>} structures Array of registered structures.
+    * @property {string} messageTemplate Template for messages.
+    * @property {string} loadingTemplate Template for widget loading.
+    *
+    * @returns {Object} self
+    */
+    this.$get = function(): any {
+      var cid = 0;
+
+      return {
+        widgets: widgets,
+        widgetsPath: widgetsPath,
+        structures: structures,
+        messageTemplate: messageTemplate,
+        loadingTemplate: loadingTemplate,
+
+        /**
+         * @ngdoc method
+         * @name adf.dashboard#id
+         * @methodOf adf.dashboard
+         * @description
+         *
+         * Creates an ongoing numeric id. The method is used to create ids for
+         * columns and widgets in the dashboard.
+         */
+        id: function(){
+          return ++cid;
+        }
+      };
+    };
+
+  });
---
+++ src/scripts/type-script/widget-content.ts
@@ -0,0 +1,159 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2015, Sebastian Sdorra
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+'use strict';
+
+angular.module('adf')
+  .directive('adfWidgetContent', function($log, $q, $sce, $http, $templateCache,
+    $compile, $controller, $injector, dashboard): void {
+
+    function parseUrl(url: string): string {
+      var parsedUrl: string = url;
+      if ( url.indexOf('{widgetsPath}') >= 0 ){
+        parsedUrl = url.replace('{widgetsPath}', dashboard.widgetsPath)
+                       .replace('//', '/');
+        if (parsedUrl.indexOf('/') === 0){
+          parsedUrl = parsedUrl.substring(1);
+        }
+      }
+      return parsedUrl;
+    }
+
+    function getTemplate(widget: any): any {
+      var deferred = $q.defer();
+
+      if ( widget.template ){
+        deferred.resolve(widget.template);
+      } else if (widget.templateUrl) {
+        // try to fetch template from cache
+        var tpl = $templateCache.get(widget.templateUrl);
+        if (tpl){
+          deferred.resolve(tpl);
+        } else {
+          var url = $sce.getTrustedResourceUrl(parseUrl(widget.templateUrl));
+          $http.get(url)
+            .success(function(response){
+              // put response to cache, with unmodified url as key
+              $templateCache.put(widget.templateUrl, response);
+              deferred.resolve(response);
+            })
+            .error(function(){
+              deferred.reject('could not load template');
+            });
+        }
+      }
+
+      return deferred.promise;
+    }
+
+    function compileWidget($scope, $element, currentScope) {
+      var model = $scope.model;
+      var content = $scope.content;
+
+      // display loading template
+      $element.html(dashboard.loadingTemplate);
+
+      // create new scope
+      var templateScope = $scope.$new();
+
+      // pass config object to scope
+      if (!model.config) {
+        model.config = {};
+      }
+
+      templateScope.config = model.config;
+
+      // local injections
+      var base = {
+        $scope: templateScope,
+        widget: model,
+        config: model.config
+      };
+
+      // get resolve promises from content object
+      var resolvers = {};
+      resolvers.$tpl = getTemplate(content);
+      if (content.resolve) {
+        angular.forEach(content.resolve, function(promise, key) {
+          if (angular.isString(promise)) {
+            resolvers[key] = $injector.get(promise);
+          } else {
+            resolvers[key] = $injector.invoke(promise, promise, base);
+          }
+        });
+      }
+
+      // resolve all resolvers
+      $q.all(resolvers).then(function(locals) {
+        angular.extend(locals, base);
+
+        // compile & render template
+        var template = locals.$tpl;
+        $element.html(template);
+        if (content.controller) {
+          var templateCtrl = $controller(content.controller, locals);
+          if (content.controllerAs){
+            templateScope[content.controllerAs] = templateCtrl;
+          }
+          $element.children().data('$ngControllerController', templateCtrl);
+        }
+        $compile($element.contents())(templateScope);
+      }, function(reason) {
+        // handle promise rejection
+        var msg = 'Could not resolve all promises';
+        if (reason) {
+          msg += ': ' + reason;
+        }
+        $log.warn(msg);
+        $element.html(dashboard.messageTemplate.replace(/{}/g, msg));
+      });
+
+      // destroy old scope
+      if (currentScope){
+        currentScope.$destroy();
+      }
+
+      return templateScope;
+    }
+
+    return {
+      replace: true,
+      restrict: 'EA',
+      transclude: false,
+      scope: {
+        model: '=',
+        content: '='
+      },
+      link: function($scope, $element) {
+        var currentScope = compileWidget($scope, $element, null);
+        $scope.$on('widgetConfigChanged', function(){
+          currentScope = compileWidget($scope, $element, currentScope);
+        });
+        $scope.$on('widgetReload', function(){
+          currentScope = compileWidget($scope, $element, currentScope);
+        });
+      }
+    };
+
+  });
