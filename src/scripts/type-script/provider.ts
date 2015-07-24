7023105860cde6faf343196bfb860905bf79e2b6
scknechaev <k.nechaev@streamcrease.com>
2015-07-24 17:54:25 +0300

    fixed error with provider.js

--- src/scripts/type-script/provider.ts
+++ src/scripts/type-script/provider.ts
@@ -2,202 +2,232 @@
 
 module Adf {
 
-    interface IDashBoardService {
-        
+    export interface IStructure {
+        rows: IRow[],
+        columns:IColumn[];
     }
 
-    export class Dashboard implements ng.IServiceProvider {
+   export interface IRow {
+        styleClass: string
+    }
 
+   export interface IColumn extends IRow {    
+    }
+
+   
+   export interface IDashBoardServiceConfig extends ng.IServiceProvider {
+        id?: number;
         widgets: any;
         structures: any;
+        messageTemplate: any;
+        loadingTemplate: any;
+        widget: (name: string, widget:any) => any;
+        structure : (name:string, structure:IStructure) => any;
+   }
+    
+
+    export class Dashboard implements IDashBoardServiceConfig {
+
+
+        widgets: any = {};
+        structures: any ={};
 
         constructor() {
-            this.widgets = {};
-            this.widgetsPath = <any>('');
-            this.structures = {};
-            this.messageTemplate = <any>('<div class="alert alert-danger">{}</div>');
-            this.loadingTemplate = <any>('\
+
+            this.messageTemplate = <any>'<div class="alert alert-danger">{}</div>';
+            this.loadingTemplate = <any>'\
       <div class="progress progress-striped active">\n\
         <div class="progress-bar" role="progressbar" style="width: 100%">\n\
           <span class="sr-only">loading ...</span>\n\
         </div>\n\
-      </div>');
+      </div>';
         }
-
-/**
-         * @ngdoc method
-         * @name adf.dashboardProvider#widget
-         * @methodOf adf.dashboardProvider
-         * @description
-         *
-         * Registers a new widget.
-         *
-         * @param {string} name of the widget
-         * @param {object} widget to be registered.
-         *
-         *   Object properties:
-         *
-         *   - `title` - `{string=}` - The title of the widget.
-         *   - `description` - `{string=}` - Description of the widget.
-         *   - `config` - `{object}` - Predefined widget configuration.
-         *   - `controller` - `{string=|function()=}` - Controller fn that should be
-         *      associated with newly created scope of the widget or the name of a
-         *      {@link http://docs.angularjs.org/api/angular.Module#controller registered controller}
-         *      if passed as a string.
-         *   - `controllerAs` - `{string=}` - A controller alias name. If present the controller will be
-         *      published to scope under the `controllerAs` name.
-         *   - `template` - `{string=|function()=}` - html template as a string.
-         *   - `templateUrl` - `{string=}` - path to an html template.
-         *   - `reload` - `{boolean=}` - true if the widget could be reloaded. The default is false.
-         *   - `resolve` - `{Object.<string, function>=}` - An optional map of dependencies which should
-         *      be injected into the controller. If any of these dependencies are promises, the widget
-         *      will wait for them all to be resolved or one to be rejected before the controller is
-         *      instantiated.
-         *      If all the promises are resolved successfully, the values of the resolved promises are
-         *      injected.
-         *
-         *      The map object is:
-         *      - `key` – `{string}`: a name of a dependency to be injected into the controller.
-         *      - `factory` - `{string|function}`: If `string` then it is an alias for a service.
-         *        Otherwise if function, then it is {@link http://docs.angularjs.org/api/AUTO.$injector#invoke injected}
-         *        and the return value is treated as the dependency. If the result is a promise, it is
-         *        resolved before its value is injected into the controller.
-         *   - `edit` - `{object}` - Edit modus of the widget.
-         *      - `controller` - `{string=|function()=}` - Same as above, but for the edit mode of the widget.
-         *      - `template` - `{string=|function()=}` - Same as above, but for the edit mode of the widget.
-         *      - `templateUrl` - `{string=}` - Same as above, but for the edit mode of the widget.
-         *      - `resolve` - `{Object.<string, function>=}` - Same as above, but for the edit mode of the widget.
-         *      - `reload` - {boolean} - true if the widget should be reloaded, after the edit mode is closed.
-         *        Default is true.
-         *
-         * @returns {Object} self
-         */
-        widget: any = function(name, widget) {
-            var w = angular.extend({ reload: false }, widget);
-            if (w.edit) {
-                var edit = { reload: true };
-                angular.extend(edit, w.edit);
-                w.edit = edit;
-            }
-            this.widgets[name] = w;
-            return this;
-        };
 
         /**
-         * @ngdoc method
-         * @name adf.dashboardProvider#widgetsPath
-         * @methodOf adf.dashboardProvider
-         * @description
-         *
-         * Sets the path to the directory which contains the widgets. The widgets
-         * path is used for widgets with a templateUrl which contains the
-         * placeholder {widgetsPath}. The placeholder is replaced with the
-         * configured value, before the template is loaded, but the template is
-         * cached with the unmodified templateUrl (e.g.: {widgetPath}/src/widgets).
-         * The default value of widgetPaths is ''.
-         *
-         *
-         * @param {string} path to the directory which contains the widgets
-         *
-         * @returns {Object} self
-         */
-        widgetsPath(path) {
-            var widgetsPath = path;
-            return this;
-        } /**
-         * @ngdoc method
-         * @name adf.dashboardProvider#structure
-         * @methodOf adf.dashboardProvider
-         * @description
-         *
-         * Registers a new structure.
-         *
-         * @param {string} name of the structure
-         * @param {object} structure to be registered.
-         *
-         *   Object properties:
-         *
-         *   - `rows` - `{Array.<Object>}` - Rows of the dashboard structure.
-         *     - `styleClass` - `{string}` - CSS Class of the row.
-         *     - `columns` - `{Array.<Object>}` - Columns of the row.
-         *       - `styleClass` - `{string}` - CSS Class of the column.
-         *
-         * @returns {Object} self
-         */
-        structure(name, structure) {
-            this.structures[name] = structure;
-            return this;
-        } /**
-         * @ngdoc method
-         * @name adf.dashboardProvider#messageTemplate
-         * @methodOf adf.dashboardProvider
-         * @description
-         *
-         * Changes the template for messages.
-         *
-         * @param {string} template for messages.
-         *
-         * @returns {Object} self
-         */
-        messageTemplate(template) {
-            this.messageTemplate = template;
-            return this;
-        } /**
-         * @ngdoc method
-         * @name adf.dashboardProvider#loadingTemplate
-         * @methodOf adf.dashboardProvider
-         * @description
-         *
-         * Changes the template which is displayed as
-         * long as the widget resources are not resolved.
-         *
-         * @param {string} loading template
-         *
-         * @returns {Object} self
-         */
-        loadingTemplate(template) {
-            this.loadingTemplate = template;
-            return this;
-        } /**
-         * @ngdoc service
-         * @name adf.dashboard
-         * @description
-         *
-         * The dashboard holds all options, structures and widgets.
-         *
-         * @property {Array.<Object>} widgets Array of registered widgets.
-         * @property {string} widgetsPath Default path for widgets.
-         * @property {Array.<Object>} structures Array of registered structures.
-         * @property {string} messageTemplate Template for messages.
-         * @property {string} loadingTemplate Template for widget loading.
-         *
-         * @returns {Object} self
-         */
-        $get() {
-            var cid = 0;
-
-            return {
-                widgets: this.widgets,
-                widgetsPath: this.widgetsPath,
-                structures: this.structures,
-                messageTemplate: this.messageTemplate,
-                loadingTemplate: this.loadingTemplate,
-
-                /**
                  * @ngdoc method
-                 * @name adf.dashboard#id
-                 * @methodOf adf.dashboard
+                 * @name adf.dashboardProvider#widget
+                 * @methodOf adf.dashboardProvider
                  * @description
                  *
-                 * Creates an ongoing numeric id. The method is used to create ids for
-                 * columns and widgets in the dashboard.
+                 * Registers a new widget.
+                 *
+                 * @param {string} name of the widget
+                 * @param {object} widget to be registered.
+                 *
+                 *   Object properties:
+                 *
+                 *   - `title` - `{string=}` - The title of the widget.
+                 *   - `description` - `{string=}` - Description of the widget.
+                 *   - `config` - `{object}` - Predefined widget configuration.
+                 *   - `controller` - `{string=|function()=}` - Controller fn that should be
+                 *      associated with newly created scope of the widget or the name of a
+                 *      {@link http://docs.angularjs.org/api/angular.Module#controller registered controller}
+                 *      if passed as a string.
+                 *   - `controllerAs` - `{string=}` - A controller alias name. If present the controller will be
+                 *      published to scope under the `controllerAs` name.
+                 *   - `template` - `{string=|function()=}` - html template as a string.
+                 *   - `templateUrl` - `{string=}` - path to an html template.
+                 *   - `reload` - `{boolean=}` - true if the widget could be reloaded. The default is false.
+                 *   - `resolve` - `{Object.<string, function>=}` - An optional map of dependencies which should
+                 *      be injected into the controller. If any of these dependencies are promises, the widget
+                 *      will wait for them all to be resolved or one to be rejected before the controller is
+                 *      instantiated.
+                 *      If all the promises are resolved successfully, the values of the resolved promises are
+                 *      injected.
+                 *
+                 *      The map object is:
+                 *      - `key` – `{string}`: a name of a dependency to be injected into the controller.
+                 *      - `factory` - `{string|function}`: If `string` then it is an alias for a service.
+                 *        Otherwise if function, then it is {@link http://docs.angularjs.org/api/AUTO.$injector#invoke injected}
+                 *        and the return value is treated as the dependency. If the result is a promise, it is
+                 *        resolved before its value is injected into the controller.
+                 *   - `edit` - `{object}` - Edit modus of the widget.
+                 *      - `controller` - `{string=|function()=}` - Same as above, but for the edit mode of the widget.
+                 *      - `template` - `{string=|function()=}` - Same as above, but for the edit mode of the widget.
+                 *      - `templateUrl` - `{string=}` - Same as above, but for the edit mode of the widget.
+                 *      - `resolve` - `{Object.<string, function>=}` - Same as above, but for the edit mode of the widget.
+                 *      - `reload` - {boolean} - true if the widget should be reloaded, after the edit mode is closed.
+                 *        Default is true.
+                 *
+                 * @returns {Object} self
                  */
-                id: () => (++cid)
+            widget= (name:string, widget) => {
+                var w = angular.extend({ reload: false }, widget);
+                if (w.edit) {
+                    var edit = { reload: true };
+                    angular.extend(edit, w.edit);
+                    w.edit = edit;
+                }
+                this.widgets[name] = w;
+                return this;
             };
+
+            /**
+                 * @ngdoc method
+                 * @name adf.dashboardProvider#widgetsPath
+                 * @methodOf adf.dashboardProvider
+                 * @description
+                 *
+                 * Sets the path to the directory which contains the widgets. The widgets
+                 * path is used for widgets with a templateUrl which contains the
+                 * placeholder {widgetsPath}. The placeholder is replaced with the
+                 * configured value, before the template is loaded, but the template is
+                 * cached with the unmodified templateUrl (e.g.: {widgetPath}/src/widgets).
+                 * The default value of widgetPaths is ''.
+                 *
+                 *
+                 * @param {string} path to the directory which contains the widgets
+                 *
+                 * @returns {Object} self
+                 */
+            widgetsPath = (path) =>{
+                this.widgetsPath = path;
+                return this;
+            };
+
+            /**
+                 * @ngdoc method
+                 * @name adf.dashboardProvider#structure
+                 * @methodOf adf.dashboardProvider
+                 * @description
+                 *
+                 * Registers a new structure.
+                 *
+                 * @param {string} name of the structure
+                 * @param {object} structure to be registered.
+                 *
+                 *   Object properties:
+                 *
+                 *   - `rows` - `{Array.<Object>}` - Rows of the dashboard structure.
+                 *     - `styleClass` - `{string}` - CSS Class of the row.
+                 *     - `columns` - `{Array.<Object>}` - Columns of the row.
+                 *       - `styleClass` - `{string}` - CSS Class of the column.
+                 *
+                 * @returns {Object} self
+                 */
+            structure = (name:string, structure:IStructure) => {
+                this.structures[name] = structure;
+                return this;
+            };
+
+            /**
+                 * @ngdoc method
+                 * @name adf.dashboardProvider#messageTemplate
+                 * @methodOf adf.dashboardProvider
+                 * @description
+                 *
+                 * Changes the template for messages.
+                 *
+                 * @param {string} template for messages.
+                 *
+                 * @returns {Object} self
+                 */
+            messageTemplate = (template) => {
+                this.messageTemplate = template;
+                return this;
+            };
+
+            /**
+                 * @ngdoc method
+                 * @name adf.dashboardProvider#loadingTemplate
+                 * @methodOf adf.dashboardProvider
+                 * @description
+                 *
+                 * Changes the template which is displayed as
+                 * long as the widget resources are not resolved.
+                 *
+                 * @param {string} loading template
+                 *
+                 * @returns {Object} self
+                 */
+            loadingTemplate = (template:string)  => {
+                this.loadingTemplate =<any> template;
+                return this;
+            };
+
+            /**
+                 * @ngdoc service
+                 * @name adf.dashboard
+                 * @description
+                 *
+                 * The dashboard holds all options, structures and widgets.
+                 *
+                 * @property {Array.<Object>} widgets Array of registered widgets.
+                 * @property {string} widgetsPath Default path for widgets.
+                 * @property {Array.<Object>} structures Array of registered structures.
+                 * @property {string} messageTemplate Template for messages.
+                 * @property {string} loadingTemplate Template for widget loading.
+                 *
+                 * @returns {Object} self
+                 */
+            $get = () => {
+                var cid = 0;
+
+                return {
+                    widgets: this.widgets,
+                    widgetsPath: this.widgetsPath,
+                    structures: this.structures,
+                    messageTemplate: this.messageTemplate,
+                    loadingTemplate: this.loadingTemplate,
+
+                    /**
+                         * @ngdoc method
+                         * @name adf.dashboard#id
+                         * @methodOf adf.dashboard
+                         * @description
+                         *
+                         * Creates an ongoing numeric id. The method is used to create ids for
+                         * columns and widgets in the dashboard.
+                         */
+                    id: function() {
+                        return ++cid;
+                    }
+                };
+            }
+
         }
-    }
-    angular.module('adf.provider', [])
+angular.module('adf.provider', [])
         .provider('dashboard', Dashboard);
+    }
 
-
-}
+ 
