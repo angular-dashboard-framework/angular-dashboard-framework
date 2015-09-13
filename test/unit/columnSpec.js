0ecdeb51b18b0ebf49e7dcb4aae889d0409d9962
Sebastian Sdorra <s.sdorra@gmail.com>
2015-09-13 13:46:13 +0200

    added some basic unit test for column

---
+++ test/unit/columnSpec.js
@@ -0,0 +1,96 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2015, Sebastian Sdorra
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+'use strict';
+
+describe('column directive tests', function() {
+
+  var $compile,
+      $rootScope,
+      $scope,
+      directive,
+      dashboard;
+
+  // Load the myApp module, which contains the directive
+  beforeEach(module('adf'));
+
+  // Store references to $rootScope and $compile
+  // so they are available to all tests in this describe block
+  beforeEach(inject(function(_$compile_, _$rootScope_, _dashboard_){
+      // The injector unwraps the underscores (_) from around the parameter names when matching
+      dashboard = _dashboard_;
+      dashboard.widgets = [];
+      dashboard.widgets['test'] = {
+        title: 'Test',
+        template: '<div class="test-widget">Hello</div>'
+      };
+
+      $compile = _$compile_;
+      $rootScope = _$rootScope_;
+      $scope = $rootScope.$new();
+      directive = '<adf-dashboard-column column="column" adf-model="adfModel" options="options" edit-mode="editMode" />';
+
+      $scope.column = {};
+      $scope.adfModel = {};
+      $scope.options = {};
+      $scope.editMode = false;
+  }));
+
+  function compileTemplate(template) {
+      // Compile a piece of HTML containing the directive
+      var el = $compile(template)($scope);
+      $scope.$digest();
+      return el;
+  }
+
+  it('should render a single column without widgets', function() {
+      var element = compileTemplate(directive);
+      expect(element.hasClass("column")).toBeTruthy();
+      expect(element.attr("adf-id").length).toBeDefined();
+      expect(element.find(".widget").length).toBe(0);
+  });
+
+  it('should render a single column with a single widget', function() {
+      $scope.column = {
+        widgets: [{
+          type: 'test'
+        }]
+      };
+
+      var element = compileTemplate(directive);
+      expect(element.hasClass("column")).toBeTruthy();
+      expect(element.find(".widget").length).toBe(1);
+      expect(element.find("div.test-widget").text()).toBe('Hello');
+  });
+
+  it('should render a single column with a nested row', function() {
+      $scope.column = {
+        rows: [{}]
+      };
+
+      var element = compileTemplate(directive);
+      expect(element.find(".row").length).toBe(1);
+  });
+
+});
