b11b4db9e9206b5729c604b4d008d0163d4aefca
Roberto Carcamo <roberto.carcamo@rc-hyperlink.it>
2015-10-07 11:59:18 +0200

    Add possibility to set collpasible status in the widget configuration

--- src/scripts/widget.js
+++ src/scripts/widget.js
@@ -72,7 +72,7 @@ angular.module('adf')
           // collapse exposed $scope.widgetState property
           if (!$scope.widgetState) {
             $scope.widgetState = {};
-            $scope.widgetState.isCollapsed = false;
+            $scope.widgetState.isCollapsed= (w.collapsed === true) ? w.collapsed : false;
           }
 
         } else {
