c8fa64dd2ea55e68e828e9d4c447ea7acb67aaad
Sebastian Sdorra <s.sdorra@gmail.com>
2016-05-13 09:48:04 +0200

    convert tabs to spaces

--- src/scripts/column.js
+++ src/scripts/column.js
@@ -102,9 +102,9 @@ angular.module('adf')
         if (widget){
           // add new item and apply to scope
           $scope.$apply(function(){
-      			if (!targetColumn.widgets) {
-      				targetColumn.widgets = [];
-      			}
+            if (!targetColumn.widgets) {
+              targetColumn.widgets = [];
+            }
             targetColumn.widgets.splice(evt.newIndex, 0, widget);
 
             $rootScope.$broadcast('adfWidgetAddedToColumn');
