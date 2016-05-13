99d526219fff1fa4388d2e5f570fa0c34803a4f5
Sebastian Sdorra <s.sdorra@gmail.com>
2016-05-13 21:07:39 +0200

    use adf directives to display structure previews

--- src/scripts/structure-preview.js
+++ src/scripts/structure-preview.js
@@ -28,6 +28,29 @@
 angular.module('adf')
   .directive('adfStructurePreview', function(adfTemplatePath) {
 
+    function adjustRowHeight(container){
+      if (container.rows && container.rows.length > 0){
+        var height = 100 / container.rows.length;
+        angular.forEach(container.rows, function(row){
+          row.style = {
+            height: height + '%'
+          }
+
+          if (row.columns){
+            angular.forEach(row.columns, function(column){
+              adjustRowHeight(column);
+            });
+          }
+        });
+      }
+    }
+
+    function prepareStructure($scope){
+      var structure = angular.copy($scope.structure);
+      adjustRowHeight(structure);
+      $scope.preview = structure;
+    }
+
     return {
       restrict: 'E',
       replace: true,
@@ -35,6 +58,7 @@ angular.module('adf')
         name: '=',
         structure: '='
       },
-      templateUrl: adfTemplatePath + 'structure-preview.html'
+      templateUrl: adfTemplatePath + 'structure-preview.html',
+      link: prepareStructure
     };
   });
