7cbe7cbc50b1bb00e3ea8f9acbc91b5f4e1085c4
Sebastian Sdorra <s.sdorra@gmail.com>
2016-06-28 20:06:21 +0200

    fix scope destruction on widgetReload and widgetConfigChanged event

--- src/scripts/widget-content.js
+++ src/scripts/widget-content.js
@@ -36,14 +36,17 @@ angular.module('adf')
     function compileWidget($scope, $element, currentScope) {
       var model = $scope.model;
       var content = $scope.content;
+
+      var newScope = currentScope;
       if (!model){
         renderError($element, 'model is undefined')
       } else if (!content){
         var msg = 'widget content is undefined, please have a look at your browser log';
         renderError($element, msg);
       } else {
-        renderWidget($scope, $element, currentScope, model, content);
+        newScope = renderWidget($scope, $element, currentScope, model, content);
       }
+      return newScope;
     }
 
     function renderWidget($scope, $element, currentScope, model, content) {
