ecbf10497a315d7585035216a7775773471f1992
Sebastian Sdorra <s.sdorra@gmail.com>
2016-06-28 20:58:33 +0200

    update sample to version 0.12.0

--- docs/js/docs-setup.js
+++ docs/js/docs-setup.js
@@ -10,7 +10,7 @@ NG_DOCS={
       "type": "service",
       "moduleName": "adf",
       "shortDescription": "The dashboard holds all options, structures and widgets.",
-      "keywords": "adf adf-dashboard-page api array checks class column columns create creates dashboard default equal holds idequal ids loading loadingtemplate messages messagetemplate method numeric ongoing options path registered service structures template widget widgets widgetspath"
+      "keywords": "active adf adf-dashboard-page api array checks class column columns create creates dashboard default equal function hold holds idequal ids loading loadingtemplate locale messages messagetemplate method numeric ongoing options passed path registered service sets setting structures template templates translation translations widget widgets widgetspath"
     },
     {
       "section": "api",
@@ -19,7 +19,7 @@ NG_DOCS={
       "type": "object",
       "moduleName": "adf",
       "shortDescription": "The dashboardProvider can be used to register structures and widgets.",
-      "keywords": "$injector adf alias angularjs api apply associated boolean bound cached called changes class classes closed collapsed column columns config configuration configured controller controlleras created creation css dashboard dashboardprovider default delimited dependencies dependency description directory displayed edit enters factory false fn frameless function html http injected injection instantiated invoke key loaded loading loadingtemplate long map messages messagetemplate method mode module modus newly object optional org passed path placeholder predefined promise promises properties published register registered registers rejected reload reloaded replaced resolve resolved resources result return row rows saved scope service sets space string structure structures styleclass template templateurl title treated true unmodified values wait widget widgetpaths widgets widgetspath"
+      "keywords": "$injector addlocale adds adf alias angularjs api apply associated boolean bound cached called category changes class classes closed code collapsed column columns config configuration configured container controller controlleras created creation css custom customwidgettemplatepath dashboard dashboardprovider default delimited dependencies dependency description directory displayed edit enters factory false fn frameless function html http injected injection instantiated invoke iso key language loaded loading loadingtemplate locale long map messages messagetemplate method mode module modus newly object optional org passed path placeholder predefined promise promises properties published register registered registers rejected reload reloaded replaced resolve resolveas resolved resources result return row rows saved scope service setlocale sets setting space string structure structures styleclass template templateurl title translations treated true unmodified values wait widget widgetpaths widgets widgetspath"
     },
     {
       "section": "api",
@@ -28,7 +28,16 @@ NG_DOCS={
       "type": "directive",
       "moduleName": "adf",
       "shortDescription": "adfDashboard is a directive which renders the dashboard with all its",
-      "keywords": "add adf adfdashboard adfmodel adfwidgetfilter api attribute button collapsible components continuous continuouseditmode dashboard default dialog directive disable div ea edit editable editmode enable enableconfirmdelete events exited false filter fire function large maximizable modal mode model object open panel remove renders required requires reset store structure true widget widgets"
+      "keywords": "add adf adfdashboard adfmodel adfwidgetfilter api attribute button categories collapsible components continuous continuouseditmode dashboard default dialog directive disable div ea edit editable editmode enable enableconfirmdelete events exited false filter fire function large maximizable modal mode model object open panel remove renders required requires reset store structure true widget widgets"
+    },
+    {
+      "section": "api",
+      "id": "adf.locale#adfLocale",
+      "shortName": "adf.locale#adfLocale",
+      "type": "object",
+      "moduleName": "adf",
+      "shortDescription": "Holds settings and values for framework supported locales",
+      "keywords": "adf adflocale api framework holds locale locales object settings supported values"
     }
   ],
   "apis": {
