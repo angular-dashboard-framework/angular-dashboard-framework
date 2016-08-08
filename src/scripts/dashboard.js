angular-dashboard-framework/angular-dashboard-framework

commit 816ab406f3ba7b9a866f7a1bd36f64dfb5786b55

order widgets in widget add dialog

src/templates/widget-add.html
@@ -6,9 +6,9 @@
   <!-- dashboard categories enabled -->
   <div ng-if="createCategories">
     <uib-accordion ng-init="categorized = createCategories(widgets)">
-      <uib-accordion-group heading="{{category}}" ng-repeat="(category, widgets) in categorized">
+      <uib-accordion-group heading="{{category.name}}" ng-repeat="category in categorized | adfOrderByObjectKey: 'name'">
         <dl class="dl-horizontal">
-          <dt ng-repeat-start="(key, widget) in widgets">
+          <dt ng-repeat-start="widget in category | adfOrderByObjectKey: 'key'">
             <a href="" ng-click="addWidget(key)">
               {{widget.title}}
             </a>
@@ -23,8 +23,8 @@
   <!-- dashboard categories disabled -->
   <div style="display: inline-block;" ng-if="!createCategories">
     <dl class="dl-horizontal">
-      <dt ng-repeat-start="(key, widget) in widgets">
-        <a href="" ng-click="addWidget(key)">
+      <dt ng-repeat-start="widget in widgets | adfOrderByObjectKey: 'key'">
+        <a href="" ng-click="addWidget(widget.key)">
           {{widget.title}}
         </a>
       </dt>

sample/index.html
@@ -139,6 +139,7 @@
     <script type="text/javascript" src="../src/scripts/widget.js"></script>
     <script type="text/javascript" src="../src/scripts/dashboard.js"></script>
     <script type="text/javascript" src="../src/scripts/structure-preview.js"></script>
+    <script type="text/javascript" src="../src/scripts/order-by-object-key.js"></script>
     <script type="text/javascript" src="../src/scripts/column.js"></script>
     <script type="text/javascript" src="../src/scripts/row.js"></script>
 

src/scripts/order-by-object-key.js
@@ -1,0 +1,39 @@
+/*
+* The MIT License
+*
+* Copyright (c) 2015, Sebastian Sdorra
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+
+/* global angular */
+angular.module('adf')
+  .filter('adfOrderByObjectKey', function($filter) {
+    'use strict';
+
+    return function(item, key){
+      var array = [];
+      angular.forEach(item, function(value, objectKey){
+        value[key] = objectKey;
+        array.push(value);
+      });
+      return $filter('orderBy')(array, key);
+    };
+  });

test/unit/orderByObjectKeySpec.js
@@ -1,0 +1,57 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2015, Sebastian Sdorra
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+'use strict';
+
+describe('adfOrderByObjectKey filter test', function() {
+
+  var $filter;
+
+  // Load the myApp module, which contains the directive
+  beforeEach(module('adf'));
+
+  // Store references to $rootScope and $compile
+  // so they are available to all tests in this describe block
+  beforeEach(inject(function(_$filter_){
+      // The injector unwraps the underscores (_) from around the parameter names when matching
+      $filter = _$filter_;
+  }));
+
+  it('should order the object', function() {
+    var object = {
+      z: {},
+      x: {},
+      c: {},
+      w: {}
+    };
+
+    var array = $filter('adfOrderByObjectKey')(object, 'key');
+    expect(angular.isArray(array)).toBeTruthy();
+    expect(array[0].key).toBe('c');
+    expect(array[1].key).toBe('w');
+    expect(array[2].key).toBe('x');
+    expect(array[3].key).toBe('z');
+  });
+
+});

commit f5fa33ff84ec2cfe912814d41cd0ecd172904c44

Closes angular-dashboard-framework/angular-dashboard-framework#257 .
Fix for regression from angular-dashboard-framework/angular-dashboard-framework@816ab406f3ba7b9a866f7a1bd36f64dfb5786b55
which broke `categories` feature.

src/scripts/dashboard.js
@@ -253,17 +253,17 @@ angular.module('adf')
      */
     function createCategories(widgets){
       var categories = {};
-      angular.forEach(widgets, function(widget){
+      angular.forEach(widgets, function(widget, key){
         var category = widget.category;
         // if the widget has no category use a default one
         if (!category){
           category = 'Miscellaneous';
         }
         // push widget to category array
-        if (!categories[category]){
-          categories[category] = [];
+        if (angular.isUndefined(categories[category])){
+          categories[category] = {widgets: {}};
         }
-        categories[category].push(widget);
+        categories[category].widgets[key] = widget;
       });
       return categories;
     }

src/templates/widget-add.html
@@ -8,8 +8,8 @@
     <uib-accordion ng-init="categorized = createCategories(widgets)">
       <uib-accordion-group heading="{{category.name}}" ng-repeat="category in categorized | adfOrderByObjectKey: 'name'">
         <dl class="dl-horizontal">
-          <dt ng-repeat-start="widget in category | adfOrderByObjectKey: 'key'">
-            <a href="" ng-click="addWidget(key)">
+          <dt ng-repeat-start="widget in category.widgets | adfOrderByObjectKey: 'key'">
+            <a href="" ng-click="addWidget(widget.key)">
               {{widget.title}}
             </a>
           </dt>

test/unit/dashboardSpec.js
@@ -340,9 +340,9 @@ describe('Dashboard Directive tests', function () {
 
         // create categories and test
         var categories = scope.createCategories(widgets);
-        expect(categories.a.length).toBe(2);
-        expect(categories.b.length).toBe(1);
-        expect(categories.Miscellaneous.length).toBe(1);
+        expect(Object.keys(categories.a.widgets).length).toBe(2);
+        expect(Object.keys(categories.b.widgets).length).toBe(1);
+        expect(Object.keys(categories.Miscellaneous.widgets).length).toBe(1);
       });
 
       it('should close add widget dialog', function(){