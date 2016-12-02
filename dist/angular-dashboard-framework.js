224936624735d7a9043a32ee34bfb641a638f0d7
Angjel Spasovski <angjel.spasovski@armedia.com>
2016-12-02 14:38:54 +0100

    AFDP-1871-647

--- dist/angular-dashboard-framework.js
+++ dist/angular-dashboard-framework.js
@@ -29,7 +29,7 @@ angular.module('adf', ['adf.provider', 'ui.bootstrap'])
   .value('adfTemplatePath', '../src/templates/')
   .value('rowTemplate', '<adf-dashboard-row row="row" adf-model="adfModel" options="options" edit-mode="editMode" ng-repeat="row in column.rows" />')
   .value('columnTemplate', '<adf-dashboard-column column="column" adf-model="adfModel" options="options" edit-mode="editMode" ng-repeat="column in row.columns" />')
-  .value('adfVersion', '0.10.1');
+  .value('adfVersion', '0.10.2');
 
 /*
 * The MIT License
